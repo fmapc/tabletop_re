6a26f47ab3c25da802f66847eef6c6ca26102734
Tiago Azevedo <tiago@fmap.tech>
2020-04-18 16:16:57 +0100

    Remove commited .bs.js from __tests__

--- __tests__/Bootstrap.bs.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-var Enzyme = require("bs-enzyme/src/Enzyme.js");
-var EnzymeAdapterReact = require("enzyme-adapter-react-16");
-
-Enzyme.configureEnzyme(new EnzymeAdapterReact());
-
-/*  Not a pure module */
